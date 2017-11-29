a4f08f4e46f8f38e227f12df01033cab3fc4457f
Charles <andrecharles4@gmail.com>
2017-11-29 13:54:12 +0100

    modifications readme'

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-#   	Test Technque Data Auchan:Direct
+#   	Test Technique Data Auchan:Direct
 
 #		SQL
 
 _1. Créer une table nommée order_details avec les colonnes suivantes :_
 
-create table order_details(order_id INTEGER, date DATE, nb_products INTEGER, amount DECIMAL(4,2), billed_price DECIMAL(4,2));
+	create table order_details(order_id INTEGER, date DATE, nb_products INTEGER, amount DECIMAL(4,2), billed_price DECIMAL(4,2));
 
 
 _2. Donner le panier moyen par jour. Le panier moyen est le montant moyen par commande sur une période donnée._
@@ -21,13 +21,13 @@ _2. Donner le panier moyen par jour. Le panier moyen est le montant moyen par co
 
 _3. Donner les 5 produits en promos les moins achetés._
 
-select p.id, sum(l.quantity)
-from products p, order_lines l
-where p.perc_promo != 0
-AND p.id = l.product_id
-group by p.id
-order by sum(l.quantity) ASC
-limit 5;
+	select p.id, sum(l.quantity)
+	from products p, order_lines l
+	where p.perc_promo != 0
+	AND p.id = l.product_id
+	group by p.id
+	order by sum(l.quantity) ASC
+	limit 5;
 
 # 	Pandas
 
--- pandas/my_script.py
+++ pandas/my_script.py
@@ -1,16 +1,18 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 orders = pd.read_csv("data/orders.csv", names = ["id", "date"])
 products = pd.read_csv("data/products.csv", header=None, names = ["product_id", "price", "perc_promo"])
 order_lines = pd.read_csv("data/order_lines.csv", header=None, names = ["order_id", "product_id", "quantity"])
 
-#Question 1 :
+#Question 1.1 :
 
 
 
 
 
-#Question 2 : Panier moyen par jour :
+#Question 1.2 : Panier moyen par jour :
 
 PanierMoyen = pd.merge(orders, order_lines, left_on = 'id', right_on = 'order_id')
 PanierMoyen = pd.merge(PanierMoyen, products, left_on='product_id', right_on='product_id')
@@ -22,12 +24,20 @@ PanierMoyen = PanierMoyen.groupby(['date'], as_index=False)['final_price'].mean(
 print("\nPanier moyen par jour :\n")
 print(PanierMoyen)
 
-#Question 3 :
+#Question 1.3 :
 
 LessSoldItems = pd.merge(order_lines, products, left_on="product_id", right_on = "product_id")
 LessSoldItems = LessSoldItems[(LessSoldItems.perc_promo != 0)]
 LessSoldItems = LessSoldItems.groupby(['product_id'], as_index=False)['quantity'].sum()
 LessSoldItems = LessSoldItems.sort_values(['quantity', 'product_id'], ascending=True)
-print("\n\nLes 5  produits avec reduction les moins achetés :\n")
+print("\n\nLes 5  produits avec reduction les moins achetes :\n")
 print(LessSoldItems['product_id'].head(5))
 print('\n')
+
+
+#Question 2 :
+
+QtyPerMonth = pd.merge(orders, order_lines, left_on="id", right_on="order_id")
+QtyPerMonth['month'] = pd.to_datetime(QtyPerMonth['date']).dt.month
+QtyPerMonth = QtyPerMonth.groupby(['month'], as_index=False)['quantity'].sum()
+print(QtyPerMonth)
